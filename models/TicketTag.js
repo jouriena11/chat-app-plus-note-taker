270f1963dcdafc8675727073d8c28e2ce13d8284
jouriena11 <117335220+jouriena11@users.noreply.github.com>
2023-02-27 21:41:18 +1100

    added more starter files, Models, and readme draft

---
+++ models/TicketTag.js
@@ -0,0 +1,41 @@
+const { Model, DataTypes } = require('sequelize');
+
+const sequelize = require('../config/connection');
+
+class TicketTag extends Model {}
+
+TicketTag.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+          },
+        ticket_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: "ticket",
+                key: "id"
+            }
+        },
+        tag_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: "tag",
+                key: "id"
+            }
+        }
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'ticket_tag',
+    }
+);
+
+module.exports = TicketTag;
