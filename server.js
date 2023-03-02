53583c8edfd48b754badc8af919b9c5402047d6b
Jackson Kent <117012325+jacko2611@users.noreply.github.com>
2023-03-02 19:29:54 +1100

    Update server.js

--- server.js
+++ server.js
@@ -40,3 +40,107 @@ app.use(routes);
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
+
+
+
+
+
+
+
+
+
+
+
+// if (process.env.NODE_ENV !== "production") {
+//     require("dotenv").config();
+//   }
+
+//   // imports
+//   const express = require("express");
+//   const bcrypt = require("bcrypt"); // for hashing passwords
+//   const app = express();
+//   const passport = require("passport");
+//   const initializePassport = require("./password-config");
+//   const flash = require("express-flash");
+//   const session = require("express-session");
+
+//   const port = 3001;
+
+//   // initialize passport
+//   initializePassport(
+//     passport,
+//     (email) => users.find((user) => user.email === email),
+//     (id) => users.find((user) => user.id === id)
+//   );
+
+//   // store information in an array
+//   const users = [];
+
+//   app.use(express.urlencoded({ extended: false }));
+//   app.use(flash());
+//   app.use(
+//     session({
+//       secret: "secret",
+//       resave: false, // don't save session if unmodified
+//       saveUninitialized: false, // don't create session until something stored
+//     })
+//   );
+//   app.use(passport.initialize());
+//   app.use(passport.session());
+
+//   // Static files
+//   app.use(express.static("public"));
+//   app.use("/css", express.static(__dirname + "public/css"));
+//   app.use("/js", express.static(__dirname + "public/js"));
+//   app.use("/img", express.static(__dirname + "public/img"));
+
+//   // Set views
+//   app.set("views", "./views");
+//   app.set("view engine", "ejs");
+
+//   // configure login post routes //also redirect me to the home page called dashboard.ejs
+//   app.post(
+//     "/login",
+//     passport.authenticate("local", {
+//       successRedirect: "/index", //redirect to the home page
+//       failureRedirect: "/login",
+//       failureFlash: true,
+//     })
+//   );
+
+//   //configure register post routes
+//   app.post("/register", async (req, res) => {
+//     try {
+//       const hashedPassword = await bcrypt.hash(req.body.password, 10);
+//       users.push({
+//         id: Date.now().toString(),
+//         name: req.body.name,
+//         email: req.body.email,
+//         password: hashedPassword,
+//       });
+//       console.log(users);
+//       res.redirect("/login");
+//     } catch {
+//       res.redirect("/register");
+//     }
+//   });
+
+//   // Routes
+//   app.get("/", (req, res) => {
+//     res.render("index");
+//   });
+
+//   app.get("/login", (req, res) => {
+//     res.render("login");
+//   });
+
+//   app.get("/register", (req, res) => {
+//     res.render("register");
+//   });
+
+//   app.get("*", (req, res) => {
+//     res.render("404");
+//   });
+
+//   // Listen on port 3000
+//   app.listen(port, () => console.log(`Listening on port ${port}`));
