92ad858a8ec7e58660ce0ed8bf9cf69b31d77ea0
Jordan O'Grady <jord_ogrady@hotmail.com>
2023-03-03 20:33:53 +1100

    Fixed seeds connection to models and added seed code in package

--- seeds/Ticket-seeds.js
+++ seeds/Ticket-seeds.js
@@ -1,8 +1,26 @@
-const { Ticket } = require('../models');
+const { Ticket } = require('../models/Ticket');
 
 const TicketData = [
- 
-];
+    {
+      title: 'My first ticket',
+      status: 'open',
+      priority: 'high',
+      user_id: 1,
+      user_content_id: 1,
+      support_user_id: 1,
+      support_user_content_id: 1
+    },
+    {
+      title: 'My second ticket',
+      status: 'closed',
+      priority: 'low',
+      user_id: 2,
+      user_content_id: 2,
+      support_user_id: 1,
+      support_user_content_id: 2
+    }
+  ];
+  
 
 const seedTicket = () => Ticket.bulkCreate(TicketData);
 
--- seeds/User-seeds.js
+++ seeds/User-seeds.js
@@ -1,9 +1,42 @@
-const { User } = require('../models');
+const bcrypt = require("bcrypt");
+const { User } = require("../models/User");
 
 const UserData = [
- 
+    {
+      username: "john_doe",
+      password: "password123",
+      first_name: "John",
+      last_name: "Doe",
+      email: "john@example.com",
+    },
+    {
+      username: "jane_doe",
+      password: "password456",
+      first_name: "Jane",
+      last_name: "Doe",
+      email: "jane@example.com",
+    },
 ];
 
-const seedUser = () => User.bulkCreate(UserData);
+const saltRounds = 10;
 
-module.exports = seedUser;
+const hashedUserData = await Promise.all(
+  UserData.map(async (user) => {
+    const hashedPassword = await bcrypt.hash(user.password, saltRounds);
+
+    return {
+      ...user,
+      password: hashedPassword,
+    };
+  })
+);
+
+module.exports = {
+    up: async (queryInterface, Sequelize) => {
+      await queryInterface.bulkInsert("users", hashedUserData);
+    },
+  
+    down: async (queryInterface, Sequelize) => {
+      await queryInterface.bulkDelete("users", null, {});
+    },
+  };
