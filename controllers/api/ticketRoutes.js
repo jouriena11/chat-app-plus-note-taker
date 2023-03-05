7b08c215d6cf1bfd671f0bf241a7de994ad915f5
jouriena11 <117335220+jouriena11@users.noreply.github.com>
2023-03-05 19:24:27 +1100

    updated ticketRoutes and messageRoutes

--- controllers/api/ticketRoutes.js
+++ controllers/api/ticketRoutes.js
@@ -1,26 +1,46 @@
 const router = require("express").Router();
-const { Ticket, Message } = require("../../models");
+const { Ticket } = require("../../models");
 
-// TODO: GET requests - get all tickets
+// GET requests - get all tickets - to be rendered on the left side of a user dashboard
+// api/ticket/all
 router.get('/all', async (req, res) => {
     try {
+        const allTickets = await Ticket.findAll();
+        res.status(200).json(allTickets);
+
+    } catch(err) {
+        res.status(500).json(err);
+    }
+})
+
+// GET requests - get a ticket by id
+// api/ticket/${id}
+router.get('/:id', async (req, res) => {
+    try {
+        const ticketData = await Ticket.findOne({
+            where: {
+                id: req.params.id
+            }
+        })
+
+        if(!ticketData) {
+            res.status(404).json({
+                message: "Ticket not found."
+            });
+            return;
+        }
+
+        res.status(200).json(ticketData);
         
     } catch(err) {
         res.status(500).json(err);
     }
 })
 
-// TODO: GET requests - get a ticket by id
-router.get('/:id', async (req, res) => {
-    try {
-
-    } catch(err) {
-        res.status(500).json(err);
-    }
-})
-
 // POST request - create a new ticket
+// api/ticket/create-ticket
 router.post('/create-ticket', async (req, res) => {
+    // TODO: to auto-specify default support_user?
     try {
         const ticketData = await Ticket.create(req.body);
         res.status(200).json(ticketData);
@@ -30,19 +50,33 @@ router.post('/create-ticket', async (req, res) => {
 })
 
 
-// TODO: UPDATE request - update ticket title, status, priority
-router.put('/:id', async (req, res) => {
+// UPDATE request - update ticket title, status, priority, and/or support_user_id
+// api/ticket/update/${id}
+router.put('update/:id', async (req, res) => {
     try {
-
+        // TODO: update ticket title
+        // TODO: update ticket status
+        // TODO: update priority
+        // TODO: update support_user_id, i.e. reassign
+    
     } catch(err) {
         res.status(500).json(err);
     }
 })
 
-// TODO: DELETE request - delete a ticket by id
-router.delete('/:id', async (req, res) => {
+// DELETE request - delete a ticket by id
+// api/ticket/delete/${id}
+router.delete('delete/:id', async (req, res) => {
     try {
-
+        const delTicket = await Ticket.destroy({
+            where: {
+                id: req.params.id
+            }
+        })
+        res.status(200).json({
+            message: "Your ticket has been deleted.",
+            rows_deleted: delTicket // 1
+        })
     } catch(err) {
         res.status(500).json(err);
     }
