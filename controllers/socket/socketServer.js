6ceee1a15555cadd712734da7f1759ff8cc1e6a0
jacko2611 <jacksonkent70@gmail.com>
2023-03-06 20:44:47 +1100

    additional socket script.

--- controllers/socket/socketServer.js
+++ controllers/socket/socketServer.js
@@ -1,10 +1,44 @@
-const io = require('socket.io')(3001, { //socket.io
+const express = require('express');
+const socketio = require('socket.io');
+const { Sequelize, DataTypes } = require('sequelize');
+const path = require('path');
+const session = require('express-session');
+
+const app = express();
+// const server = http.createServer(app);
+
+//
+const io = require('socket.io')(3001, { 
     cors: {
         origin: ["http://localhost:3001"],
     },
 });
 
-io.on('connection', (socket) => { //connection to socket
+//Creating a database connection
+const sequelize = new Sequelize('database', 'user', 'password', {
+    host: 'localhost',
+    dialect: 'mysql'
+  });
+
+// Define a model for chat messages
+const ChatMessage = sequelize.define('ChatMessage', {
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    message: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    read: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: false
+    }
+  });
+
+  // Connection to socket.io
+  io.on('connection', (socket) => {
     console.log(socket.id);
     socket.on("send-message", (message, chat) => { //send-message event
         if (chat === '') {
